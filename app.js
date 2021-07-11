1ed8cdce7065a5c2b8940b822e84133306620237
ryanjgani <ryanscar9@gmail.com>
2021-07-11 13:44:41 +0800

    add start script

--- app.js
+++ app.js
@@ -98,6 +98,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-    console.log("Connected to Localhost 3000...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on Port ${port}...`);
 });
