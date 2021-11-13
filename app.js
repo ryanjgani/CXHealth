fb50af45bf44bec5a2040f216e58cb7d2ad7f935
ryanjgani <ryanscar9@gmail.com>
2021-11-13 14:18:04 +0800

    backend changes

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Serving on Port ${port}...`);
 });
